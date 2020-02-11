5b80b7f564c7434a2aeb7be43b7fa3abaca2e583
davebrown1975 <dave@tucanoo.com>
2020-02-11 14:51:15 +0100

    Initial Commit

---
+++ src/build/precache-manifest.d6715468f57697add77c8f846bc498a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fd73d30b335566070e87ecf3a49367",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0dda28300f8cda2aae7",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9762f4475bb8681aa1a2",
+    "url": "/static/js/2.6797f984.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.6797f984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0dda28300f8cda2aae7",
+    "url": "/static/js/main.ae5bcd77.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,90 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { MongoClient } from 'mongodb'
+import path from 'path'
+
+const dbUrl =
+  'mongodb+srv://dave:ferret@cluster0-gojph.mongodb.net/test?retryWrites=true&w=majority'
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json())
+
+const withDB = async ( operations, res ) => {
+  try {
+    
+    const client = await MongoClient.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+    const db = client.db('my-blog')
+    
+    await operations(db);
+
+    client.close()
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: ' error ', error: error })
+  }
+}
+
+
+app.get('/api/articles/:name', async (req, res) => {
+  console.log('called get')
+  withDB(async (db) => {
+
+    const articleName = req.params.name    
+    const articleInfo = await db
+    .collection('articles')
+    .findOne({ name: articleName })
+    
+    res.status(200).json(articleInfo)
+  }, res)
+    
+})
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  const articleName = req.params.name
+
+  withDB(async (db) => {
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    console.log('updating')
+    await db.collection('articles').updateOne({
+      name: articleName },{
+      '$set': {
+        upvotes: articleInfo.upvotes + 1
+      },
+    });
+
+    console.log('updated')
+    const updateArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+    res.status(200).json(updateArticleInfo);
+  }, res);
+    
+})
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const articleName = req.params.name
+  const { username, text } = req.body
+
+  withDB(async (db) => {
+   
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    await db.collection('articles').updateOne({
+      name: articleName },{
+      '$set': {
+        comments: articleInfo.comments.concat({ username, text })
+      },
+    });
+    console.log('updated')
+    const updateArticleInfo = await db.collection('articles').findOne({ name: articleName });
+   
+    res.status(200).json(updateArticleInfo);
+  }, res);
+})
+
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+})
+
+app.listen(8000, () => console.log('Listening on 8000'))
